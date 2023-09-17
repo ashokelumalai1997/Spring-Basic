89ee13bf34f48f7851548a2f15be6af37e1da776
ASHOK E <ashoke@ASHOKs-Air.alphionsee.in>
2023-09-17 23:26:40 +0530

    Added pacman game

--- src/main/java/com/project/spring/AppGamingBasic.java
+++ src/main/java/com/project/spring/AppGamingBasic.java
@@ -1,12 +1,12 @@
 package com.project.spring;
 
-import com.project.spring.game.MarioGame;
-import com.project.spring.game.SuperContraGame;
+import com.project.spring.game.PacManGame;
 
 public class AppGamingBasic {
     public static void main(String[] args){
         //var game = new MarioGame();
-        var game =  new SuperContraGame();
+        //var game =  new SuperContraGame();
+        var game =  new PacManGame();
         //tight coupling of Gamerunner to SuperContraGames
         // Coupling - how much work in changing something
         var gameRunner = new GameRunner(game);
---
+++ src/main/java/com/project/spring/game/PacManGame.java
@@ -0,0 +1,16 @@
+package com.project.spring.game;
+
+public class PacManGame implements GamingConsole{
+    public void up(){
+        System.out.println("UP");
+    }
+    public void down(){
+        System.out.println("DOWN");
+    }
+    public void left(){
+        System.out.println("LEFT");
+    }
+    public void right(){
+        System.out.println("RIGHT");
+    }
+}
