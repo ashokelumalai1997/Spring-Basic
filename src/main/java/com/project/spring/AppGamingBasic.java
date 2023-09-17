071f2ed23ed1e345434fcd497db80cbe813c9af7
ASHOK E <ashoke@ASHOKs-Air.alphionsee.in>
2023-09-17 23:14:38 +0530

    Tight coupling example

--- src/main/java/com/project/spring/AppGamingBasic.java
+++ src/main/java/com/project/spring/AppGamingBasic.java
@@ -7,6 +7,7 @@ public class AppGamingBasic {
     public static void main(String[] args){
         var marioGame = new MarioGame();
         var superContraGame =  new SuperContraGame();
+        //tight coupling of Gamerunner to SuperContraGames
         var gameRunner = new GameRunner(superContraGame);
         gameRunner.run();
     }
