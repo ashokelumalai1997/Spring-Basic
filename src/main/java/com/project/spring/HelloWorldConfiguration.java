beb6d74ee0cbd8104f29aeb998059c35fa3c13ca
ASHOK E <ashoke@ASHOKs-Air.alphionsee.in>
2023-09-18 08:20:30 +0530

    Spring different types of Bean creation

--- src/main/java/com/project/spring/HelloWorldConfiguration.java
+++ src/main/java/com/project/spring/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-record Person(String name, int age) {};
+record Person(String name, int age, Address address) {};
 record Address(String firstLine, String city){};
 @Configuration
 public class HelloWorldConfiguration {
@@ -18,7 +18,16 @@ public class HelloWorldConfiguration {
     }
     @Bean
     public Person person(){
-        return new Person("Ravi",20);
+        return new Person("Ravi",20,new Address("NA","abc"));
+    }
+    @Bean
+    public Person person2MethodCall(){
+        return new Person(name(),age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address2){//name,age,address2
+        return new Person(name,age, address2);
     }
 
     @Bean(name="address2")
